21c12f12b76e27c284dccba539528985f315d0da
Joel Nada <joelnada789@gmail.com>
2023-05-30 21:28:45 +0530

    test2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://joelnada.github.io/joel-ai-chat">
       <App />
     </BrowserRouter>
   </React.StrictMode>
